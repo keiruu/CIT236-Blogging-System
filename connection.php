77daff3d9c8c8c07aab1c2c2550855c5bcb9e44d
znrick <zenrick.parcon@wvsu.edu.ph>
2022-01-24 16:11:36 +0800

    database

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    $serverName = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dataBase = 'bloggingsystem';
+    
+
+    $connect = mysqli_connect($serverName, $username, $password, $dataBase);
+    
+?>
