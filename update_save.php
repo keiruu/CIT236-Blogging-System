dcfe66abdb067c809ae11fa036b15b410c551763
Seynzs <thrys.formoso@wvsu.edu.ph>
2022-01-30 15:56:44 +0800

    Update on admin side
    Connected whoever is logged in.

--- update_save.php
+++ update_save.php
@@ -9,16 +9,18 @@
     $updateUser->bindparam(':username', $_POST['username']);
     $updateUser->bindparam(':password', $_POST['password']);
     $updateUser->bindparam(':email', $_POST['email']);
-    
     $updateUser->execute();
+    $adminid = $_GET['adminid'];
+    $usersql = $conn->prepare ("Select * from user where userID = '$adminid'");
 
-
+	$usersql->execute();
+	$user = $usersql->fetch();
 	echo "
     	<script>
     		alert ('Sucessfullly Updated');
     	</script>
 		";
 
-        header ("Location: admin_panel.php");
+        header ("Location: admin_home.php?uid=0&adminid=".$user['userID']);
 
  ?>
